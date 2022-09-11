532fcf2bcfa32904857d205650b15e7011e00266
Yahya Oukharta <youkharta@gmail.com>
2022-09-12 00:08:41 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "micro.hpp"
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+
+    Microboard b;
+    
+    
+    b.print();
+    b.move(0);
+    b.print();
+    b.move(1);
+    b.print();
+    b.move(4);
+    b.print();
+    b.move(2);
+    b.print();
+    b.move(8);
+    b.print();
+    //b.move(8);
+}
---
+++ micro.hpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+enum PlayerId
+{
+    PLAYER_X,
+    PLAYER_O
+};
+
+enum GameStatus
+{
+    X_WON,
+    O_WON,
+    TIE,
+    IN_PROGRESS
+};
+
+static int win_masks[8] = {0x7, 0x38, 0x1c0, 0x49, 0x92, 0x124, 0x111, 0x54};
+
+class Microboard
+{
+public:
+    int xboard;
+    int oboard;
+    GameStatus status;
+    PlayerId playerToPlay;
+
+    Microboard() // fresh board
+    {
+        xboard = 0b000000000;
+        oboard = 0b000000000;
+        status = IN_PROGRESS;
+        playerToPlay = PLAYER_X;
+    }
+
+    Microboard(Microboard const &b) // cloning
+    {
+        xboard = b.xboard;
+        oboard = b.oboard;
+        status = b.status;
+        playerToPlay = b.playerToPlay;
+    }
+    Microboard(Microboard const &b, int m) // cloning and applying bove
+    {
+        xboard = b.xboard;
+        oboard = b.oboard;
+        status = b.status;
+        playerToPlay = b.playerToPlay;
+        move(m);
+    }
+
+    int maskOfMove(int m) const
+    {
+        return 1 << (8 - m);
+    }
+
+    vector<int> available_moves() const
+    {
+        vector<int> moves;
+        int full_board = xboard | oboard;
+
+        for (int i = 0; i < 9; ++i)
+        {
+            if ((full_board & maskOfMove(i)) == 0)
+                moves.push_back(i);
+        }
+        return moves;
+    }
+
+    void move(int m)
+    {
+        if (playerToPlay == PLAYER_X)
+        {
+            xboard += maskOfMove(m);
+            playerToPlay = PLAYER_O;
+        }
+        else
+        {
+            oboard += maskOfMove(m);
+            playerToPlay = PLAYER_X;
+        }
+        status = getStatus();
+    }
+
+    GameStatus getStatus() const {
+        for (int i = 0; i < 8; ++i){
+            if ((xboard & win_masks[i]) == win_masks[i])
+                return X_WON;
+            else if((oboard & win_masks[i]) == win_masks[i])
+                return O_WON;
+        }
+        if (available_moves().size() == 0) return TIE;
+        return IN_PROGRESS;
+    }
+
+    void print() const {
+        for (int y = 0; y < 3; ++y){
+            for (int x = 0; x < 3; ++x){
+                if ((xboard & maskOfMove(y*3+x)))
+                    cout << "X ";
+                else if ((oboard & maskOfMove(y*3+x)))
+                    cout << "O ";
+                else 
+                    cout << "- ";
+            }   
+            cout << endl;
+        }
+        cout << "playerToPlay = " << (playerToPlay == PLAYER_X ? "X" : "O") << endl;
+
+        vector<int> moves = available_moves();
+        for (int i = 0;i<moves.size(); ++i){
+            cout << moves[i] << " ";
+        }
+        cout << endl;
+        cout << "status = " << status << endl << endl;
+    }
+};
